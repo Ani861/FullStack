c901f471c60b8b6857ced631731c9fafe2d0bca7
Ani861 <gkaniruddha26@gmail.com>
2026-03-08 11:34:25 +0530

    create routes/auth.js

---
+++ Backend/routes/authroutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router()
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+router.post("/login", async(req,res)=>{
+
+const {email,password}=req.body
+
+const user = await User.findOne({email})
+
+if(!user)
+return res.status(400).json("User not found")
+
+const valid = await bcrypt.compare(password,user.password)
+
+if(!valid)
+return res.status(400).json("Invalid password")
+
+const token = jwt.sign(
+{id:user._id,role:user.role},
+process.env.JWT_SECRET
+)
+
+res.json({token,user})
+
+})
+
+module.exports = router
